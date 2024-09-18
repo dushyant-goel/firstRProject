b278cab793fdd1bee48e265a518bc6a32ef8226e
Dushyant Goel <dushyant95goel@gmail.com>
2024-09-19 00:24:15 +0100

    assignment - 1 files

---
+++ mySecondRScript.R
@@ -0,0 +1,15 @@
+myFirstRFunc <- function(n) {
+  # Takes a number as an input and outputs a number
+  
+  stopifnot(is.numeric(n), n%%1==0, n>=0) # Stop if the input is not a non-negative integer.
+  
+  acc <- 0;
+  for(i in 0:n-1) {
+    if(i %% 2 == 0 | i %% 7 == 0) { # Check divisibility
+      acc <- acc + i
+    }
+  }
+  
+  return(acc) # return the sum
+}
+
