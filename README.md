e5f07ac39e1811ef62f6f18f5f2c0032930ab0c6
Dushyant Goel <dushyant95goel@gmail.com>
2024-10-26 22:25:29 +0100

    Assignment-1 Review

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# firstRProject
-This is a repo for an R project
+# Assignment - 1 
+
+This is a repo for the first assignment.     
+Statistical Computation and Empirical Methods,   
+Autum 2024.
--- mySecondRScript.R
+++ mySecondRScript.R
@@ -1,15 +1,14 @@
-myFirstRFunc <- function(n) {
+my_first_r_func <- function(n) {
   # Takes a number as an input and outputs a number
-  
-  stopifnot(is.numeric(n), n%%1==0, n>=0) # Stop if the input is not a non-negative integer.
-  
-  acc <- 0;
-  for(i in 0:n-1) {
-    if(i %% 2 == 0 | i %% 7 == 0) { # Check divisibility
+
+  stopifnot(is.numeric(n), n %% 1 == 0, n >= 0) 
+  # Stop if the input is not a non-negative integer.
+  acc <- 0
+  for (i in 0:n - 1) {
+    if(i %% 2 == 0 || i %% 7 == 0) { # Check divisibility
       acc <- acc + i
     }
   }
-  
+
   return(acc) # return the sum
 }
-
